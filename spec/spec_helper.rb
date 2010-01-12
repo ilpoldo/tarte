d836b2f1b67ba3c6a536d0db1d01643fb9bd9acb
Leandro Pedroni <ilpoldo@gmail.com>
2010-01-12 17:22:07 +0000

    spec support folder loading

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,6 +27,10 @@ require 'tarte'
 require 'spec'
 require 'spec/autorun'
 
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
+
 Spec::Runner.configure do |config|
   
 end
